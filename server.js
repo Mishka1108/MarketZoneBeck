716c49f8174080f9412dd43f0f314f468c5bb789
Mishka1108 <mr.mishka3003@gmail.com>
2025-06-10 10:51:25 -0700

    ping1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
-// const pingRouter = require("./routes/ping");
+
 
 dotenv.config();
 
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 // Middleware to parse URL-encoded data (important for form submissions)
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-// app.use('/ping', pingRouter);
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/admin", require("./routes/admin"));
 app.use("/api/users", require("./routes/user"));
